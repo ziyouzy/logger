01d5f71789222bf27454913a400fa620d1c2fa92
ziyouzy <603708556@qq.com>
2021-02-28 20:23:06 -0800

    封装完成

---
+++ go.mod
@@ -0,0 +1,5 @@
+module logger
+
+go 1.15
+
+require github.com/phachon/go-logger v0.0.0-20191215032019-86e4227f71ea // indirect
---
+++ logger_test.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	"time"
+    "testing"	
+)
+
+
+ 
+
+func TestLogger(t *testing.T) {
+	defer Destory()
+
+	Error("test_Error")
+	Errorf("test_Errorf|%s,%s","成功",time.Unix(0, time.Now().UnixNano()).Format("2006-01-02 15:04:05.000000000"))
+	Warning("test_Warning")
+	Warningf("test_Warningf|%s,%s","成功",time.Unix(0, time.Now().UnixNano()).Format("2006-01-02 15:04:05.000000000"))
+    Info("test_Info")
+	Infof("test_Infof|%s,%s","成功",time.Unix(0, time.Now().UnixNano()).Format("2006-01-02 15:04:05.000000000"))
+	Debug("test_Debug")
+	Debugf("test_Debugf|%s,%s","成功",time.Unix(0, time.Now().UnixNano()).Format("2006-01-02 15:04:05.000000000"))
+}
